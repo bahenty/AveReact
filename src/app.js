0e4ce9ec4ea1d00384302e123802f9fd78b6022f
bahenty <bahenty@gmail.com>
2018-08-04 18:13:01 +0200

    ui sass button

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AppRouter from './routers/AppRouter'
-
 import 'normalize.css'
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
--- src/components/Content.js
+++ src/components/Content.js
@@ -7,6 +7,7 @@ class Content extends React.Component {
     return (
       <div id='my-content'>
         <p>content</p>
+        <button className="btn">Shop men's collection</button>
       </div>
     )
   }
